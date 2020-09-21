334fd0d874c21e18109fb9563e818c13a5efb717
elohim-meth <elohim-meth@yandex.ru>
2020-09-21 17:57:01 +0300

    STD test

---
+++ tests/test_std_ns.cpp
@@ -0,0 +1,12 @@
+﻿#define DOCTEST_CONFIG_VOID_CAST_EXPRESSIONS
+#include <doctest/doctest.h>
+#include <rtti/metadefine.h>
+
+TEST_CASE("vector of int")
+{
+    auto il = {1, 2, 3, 4, 5, 6, 7};
+    rtti::variant tmp = il;
+
+    auto *meta_class = rtti::MetaNamespace::global()->getNamespace("std")->getClass("vector<int>");
+    REQUIRE(meta_class);
+}
